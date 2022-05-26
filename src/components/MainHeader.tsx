a2601fc1e71154ff6a57cbca257ad27672c86763
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2022-05-26 15:39:33 -0700

    Remove extra complexity.

--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -1,31 +1,19 @@
-import { Anchor, Box, Header, Menu, Nav, ResponsiveContext } from "grommet";
+import { Anchor, Box, Header, Nav, Text } from "grommet";
 import React from "react";
+import { Link } from "react-router-dom";
 
 export function MainHeader() {
   return (
     <Header pad="medium">
       <Box direction="row" justify="between" fill>
-        <Anchor href="/" label="Mamquam Meltdown" />
-        <ResponsiveContext.Consumer>
-          {(responsive) =>
-            responsive === "small" ? (
-              <Menu
-                label="Click me"
-                items={[
-                  {
-                    label: "Results",
-                    onClick: () => {},
-                  },
-                ]}
-              />
-            ) : (
-              <Nav direction="row">
-                <Anchor href="/info" label="Info" />
-                <Anchor href="/results" label="Results" />
-              </Nav>
-            )
-          }
-        </ResponsiveContext.Consumer>
+        <Link to="/">
+          <Text>Mamquam Meltdown</Text>
+        </Link>
+        <Nav direction="row">
+          <Link to="/results">
+            <Anchor>Results</Anchor>
+          </Link>
+        </Nav>
       </Box>
     </Header>
   );
--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -8,7 +8,6 @@ export const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="info" element={<Info />} />
       <Route path="results" element={<Results />} />
     </Routes>
   );
